b920bb69bde6f1f467a528611c9d0b2a718de9c3
Dibyajyoti Behera <dibyajyoti.a.behera@accenture.com>
2018-06-20 14:08:21 +0530

    module 12  completed

--- app/events/common/simpleModal.component.ts
+++ app/events/common/simpleModal.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef, Inject } from "@angular/core";
+import { JQ_TOKEN } from "./jQuery.service";
 
 @Component(
     {
         selector : 'simple-modal',
         template : `
-            <div id="simple-modal" class="modal fade" tabindex="-1">
+            <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -15,7 +16,7 @@ import { Component, Input } from "@angular/core";
                                 {{title}}
                             </h4>
                         </div>
-                        <div class="modal-body">
+                        <div class="modal-body" (click)="closeModal()">
                             <ng-content></ng-content>
                         </div>
                     </div>
@@ -28,9 +29,20 @@ import { Component, Input } from "@angular/core";
                 
             `
         ]
-    }
+    } 
 
 )
 export class SimpleModalComponent {
     @Input() title : string;
+    @Input() elementId : string;
+    @Input() closeBodyOnClick : string;
+    @ViewChild('modalcontainer') containerElement : ElementRef; 
+    constructor(@Inject(JQ_TOKEN) private jquery : any){}
+    closeModal(){
+        if(this.closeBodyOnClick === 'true')
+            {
+                this.jquery(this.containerElement.nativeElement).modal('hide');
+            }
+    }
+
 }
